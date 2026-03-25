d7338cac9651e9906ec3e52c291ec5569e75604d
Frank Harvy Astaiza Oviedo <36683463+Fhaz5000@users.noreply.github.com>
2026-03-25 17:52:57 -0500

    Create Sorteo model with Mongoose schema

---
+++ src/models/Sorteo.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const SorteoSchema = new mongoose.Schema({
+  Nombre:               String,
+  FechaEvento:          Date,
+  Valor:                Number,
+  OportunidadesPorBoleta: Number,
+  CantidadCifras:       Number,
+  TemaId:               mongoose.Schema.Types.ObjectId,
+  Activo:               Boolean,
+  EstaEliminado:        Boolean
+}, { collection: "sorteos" })   // ajusta el nombre de la colección si es diferente
+
+module.exports = mongoose.model("Sorteo", SorteoSchema)
